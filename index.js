90a2a35a9cacf704938fb5af9f25efc3b06df8c2
Chrispylcfc <chrispylcfc@gmail.com>
2019-03-14 18:27:47 +0000

    Node App with Test

---
+++ index.js
@@ -0,0 +1,17 @@
+// Importing Node Framework
+// const express = require('express');
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+// Respond with Hello World for requests that hit '/'
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+app.listen(PORT);
+
+console.log(`Application Server listening on port ${PORT} in your browser.`);
+
+module.exports = app;
